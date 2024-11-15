ce6f10c5fdb5199ffdece280a94cb58bd09ea45e
MiguelRosales1264 <miguelrosales1264@gmail.com>
2024-11-15 10:12:56 -0700

    initial commit

---
+++ pytorch_simple_fullynet.py
@@ -0,0 +1,24 @@
+# Imports
+import torch # entire library
+import torch.nn as nn # all nn modules (linear, cnn, loss functions, etc
+import torch.optim as optim # optimization algorithms (stochastic gradient descent, etc)
+import torch.nn.functional as F # activation functions (w/o parameters), also in nn package (relu, tanh, etc)
+from torch.utils.data import DataLoader # easier dataset management, helps create minibacthes for training
+import torchvision.datasets as datasets # helps import datasets
+import torchvision.transforms as transforms # transformations to perform on dataset
+
+# Create Fully Connected Network
+
+# Set device
+
+# Hyperparameters
+
+# Load data
+
+# Initialize network
+
+# Loss and optimizer
+
+# Train network
+
+# Check accuracy on training & test to see how good our model is
